d173ea683c4b5dc397a52e12bae519389feff94b
Amanda M. Jessup <mandajessup@gmail.com>
2021-02-28 16:14:17 -0500

    Add project files.

---
+++ SloppyJoe/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SloppyJoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+
+            MenuItem[] menuItems = new MenuItem[5];
+            string guacamolePrice;
+
+            for (int i = 0; i < 5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >=3)
+                {
+                    menuItems[i].breads = new string[]
+                    {
+                        "Plain Bagel",
+                        "Buttered Sesame Bagel",
+                        "Onion Bagel",
+                        "Blueberry Bagel",
+                    };
+                }
+
+                menuItems[i].Generate();
+            }
+
+            item1.Text = menuItems[0].Description;
+            price1.Text = menuItems[0].Price;
+            item2.Text = menuItems[1].Description;
+            price2.Text = menuItems[1].Price;
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                proteins = new string[]
+                {
+                    "Vegan Roast Beef",
+                    "Smart Dog",
+                    "Portabella Mushroom Patty"
+                },
+
+                breads = new string[]
+                {
+                    "Cajun Pita Wrap",
+                    "Ethiopian Njera",
+                    "Organic Dinner Roll"
+                },
+
+                condiments = new string[]
+                {
+                    "Grey Pupon",
+                    "Raspberry Tartar",
+                    "Spicy Ranch"
+                } 
+            };
+            specialMenuItem.Generate();
+
+            item6.Text = specialMenuItem.Description;
+            price6.Text = specialMenuItem.Price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = "Add guacamole for " + guacamolePrice;
+
+
+
+        }
+    }
+}
---
+++ SloppyJoe/MenuItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SloppyJoe
+{
+    class MenuItem
+    {
+        public static Random randomizer = new Random();
+        public string[] proteins =
+        {
+            "Roast Beef",
+            "Salami",
+            "Turkey",
+            "Ham",
+            "Pastrami",
+            "Tofu"
+        };
+
+        public string[] condiments =
+        {
+            "Yellow Mustard",
+            "Brown Mustard",
+            "Honey Mustard",
+            "Mayo",
+            "Relish",
+            "French Dressing"
+
+        };
+
+        public string[] breads =
+        {
+            "Rye",
+            "White",
+            "Wheat",
+            "Pumpernickel",
+            "Sourdough"
+        };
+
+        public string Description = "";
+        public string Price;
+
+
+        public void Generate()
+        {
+            string randomProtein = proteins[randomizer.Next(proteins.Length)];
+            string randomCondiment = condiments[randomizer.Next(condiments.Length)];
+            string randomBread = breads[randomizer.Next(breads.Length)];
+            Description = randomProtein + " with " + randomCondiment + " on " + randomBread;
+
+            decimal bucks = randomizer.Next(2, 5);
+            decimal cents = randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+
+        }
+
+
+
+    }
+}
